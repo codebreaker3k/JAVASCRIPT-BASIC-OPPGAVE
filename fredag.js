12d2eaf26b74f759cd2f306a8b1cb220d433f004
codebreaker3k <royterjestrommen@hotmail.com>
2023-11-22 12:38:47 +0100

    *

---
+++ fredag.js
@@ -0,0 +1,68 @@
+console.log("Hello Matrix");
+
+let x = 10;
+const y = 5;
+let z = x + y;
+console.log(z);
+
+let b = 1;
+console.log(b++);
+console.log(b);
+
+let c = 1;
+console.log(c--);
+console.log(c);
+
+let a = 2;
+let d = a * a;
+console.log(d);
+
+let o = 3;
+let p = o * o;
+console.log(p);
+
+/*function myMultiply()
+/*{
+let x= $('#num1').val();
+let y= x*x;
+alert(x+" times "+x+" equals "+y);
+return false;
+}
+console.log(answer);
+*/
+
+let h = 20;
+let g = 18;
+if (h > g);
+else g > h;
+console.log(h > g);
+
+/*let age = 18
+const userAge = userAge ? userAge = "Myndig" : userAge = "Barn"
+console.log(userAge)
+*/
+
+/*
+let userAge = prompt("Er du gammel nok til å stemme?", "");
+
+let borger = 20
+let E = 19
+let R = 6
+if (borger === 'E' && userAge >= 18) {
+  alert("Du kan stemme!");
+} else { (borger === `R` && userAge <= 6)
+  alert("Du er bebissss");
+}*/
+
+function isEven(num) {
+  num%0 === 2 ? 
+  console.log("odd") : console.log("even");
+}
+isEven(1);
+
+function isEven
+if ((x % 2) == 0) {
+    console.log (even)
+ } else {
+    console.log (odd)
+ }
---
+++ super.js
@@ -0,0 +1,100 @@
+
+//? Svaret på hver av oppgavene skal konsoll logges
+
+//* Assignment 1: Logging to the Console
+
+//! log into the console: Hello world!
+
+//* Assignment 2: Variables
+// Let defines a variable that can be changed later.
+let x = 10;
+
+// Const defines a variable that cannot be changed later.
+const y = 5;
+
+/* Creating a variable to shorten code The 2 variables above.
+Output should be: "Resultatet er: 15" (hvor 15 er summen av variablene) */
+
+//! Write code here
+
+//* Assignment 3:
+// Use double operators to solve the parts below:
+
+// Incrementing a number by 1.
+
+//! Write code here
+
+// Decrementing a number by 1.
+
+//! Write code here
+
+// Multiplying a number by itself.
+
+//! Write code here
+
+// Dividing a number by itself.
+
+//! Write code here
+
+//* Assignment 4:
+/* Make an if else statement that returns a random string */
+
+//! Write code here
+
+//* Assignment 5:
+/* Make a function that uses a parameter to check if a number is odd or even */
+
+//! Write code here
+
+//* Assigment 6:
+/* Make a variable called userTitle and set the content equal to "Mr." if userMale is true,
+or to "Mrs." if userMale is false. Use the TERNARY conditional to do this:
+
+const variable = statement ? "this if true" : "this if not true"
+
+Try changing userMale to both true and false and console.log your new variable,
+to see that your conditional is working. */
+
+//! Write code here
+
+//* Assigment 8:
+/*
+Write a function that takes in 2 parameters:
+
+ - A name (string)
+ - An hour of the day (number)
+
+The function should return:
+"Good night (name received)" if the hour received is 0-6
+"Good morning (name received)" if the hour received is 6-12
+"Good day (name received)" if the hour received is 12-18
+"Good evening (name received)" if the hour received is 18-24
+"Invalid time" if the hour received is greater than 24
+
+Use whichever function syntax you want.
+Use template literal in your return: `This is a template literal ${variable}`
+
+Example console.log(yourFunction("Gunnar", 19)) should console log Good evening Gunnar
+Console log the function a few times to show that it's working. */
+
+//! Write code here
+
+
+//* Assignment 9:
+/* Write a function that takes in 2 numbers as parameters.
+
+In your function, check which of the numbers received is the largest,
+then return the sum of the largest number divided by the smaller.
+
+Use arrow function syntax.
+
+Example: your function receives 13 and 24, it should return the sum of 24/13 */
+
+//! Write code here
+
+
+//* Assignment 10:
+/* Make a function that uses Switch/Case or if/else,
+Random Number Generator, Methods, and Arrays with Objects */
+
+//! Write code here
